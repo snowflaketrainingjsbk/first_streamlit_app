1750781e3a5d1697a6f3e314f39f7f0baa277c29
snowflaketrainingjsbk <139780240+snowflaketrainingjsbk@users.noreply.github.com>
2023-07-17 12:57:59 -0300

    Update streamlit_app.py
    Change header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("Hello World")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
