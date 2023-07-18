407964cc030d4a37d8c5b5f8ed119758d600eb8a
snowflaketrainingjsbk <139780240+snowflaketrainingjsbk@users.noreply.github.com>
2023-07-18 09:25:16 -0300

    Update streamlit_app.py
    remove test print

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 # Normalizes json into a a flat table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the normalized table
